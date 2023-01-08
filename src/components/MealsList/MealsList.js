0cf328cb81aa1954080a17f9019dcee81d806057
Teodor Todorov <ttodorov.jp@gmail.com>
2023-01-08 10:36:36 +0200

    Moved files from JS version to TS version and deployed to Firebase

---
+++ src/components/MealsList/MealsList.js
@@ -0,0 +1,44 @@
+import classes from "./MealsList.module.css";
+import Card from "../UI/Card";
+
+const MealsList = (props) => {
+  const DUMMY_MEALS = [
+    {
+      id: "m1",
+      name: "Sushi",
+      description: "Finest fish and veggies",
+      price: 22.99,
+    },
+    {
+      id: "m2",
+      name: "Schnitzel",
+      description: "A german specialty!",
+      price: 16.5,
+    },
+    {
+      id: "m3",
+      name: "Barbecue Burger, Barbecue Burger, Barbecue Burger, Barbecue Burger, Barbecue Burger, Barbecue Burger",
+      description: "American, raw, meaty",
+      price: 12.99,
+    },
+    {
+      id: "m4",
+      name: "Green Bowl",
+      description: "Healthy...and green...",
+      price: 18.99,
+    },
+  ];
+  const meals = DUMMY_MEALS.map((meal) => (
+    <Card key={meal.id} additionalClass="meals-list">
+      <div className={classes["meals-list"]}>
+        <p>{meal.name}</p>
+        <p>{meal.price}</p>
+        <p>{meal.description}</p>
+      </div>
+    </Card>
+  ));
+
+  return <div>{meals}</div>;
+};
+
+export default MealsList;
