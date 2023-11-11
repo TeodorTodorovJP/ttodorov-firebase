950f15ba51f44fb6ae60a6d3e57e060eebdd488b
Teodor Todorov <ttodorov.jp@gmail.com>
2023-11-11 06:35:16 +0200

    Notes Texts
    All texts needed for the Notes app

--- src/components/Notes/notesTexts.ts
+++ src/components/Notes/notesTexts.ts
@@ -5,34 +5,115 @@ export interface Langs {
 
 interface Lang {
   main: Main
+  error: Error
+  onDeleteNote: OnDeleteNote
+  onDeleteTag: OnDeleteTag
+}
+
+interface Error {
+  title: string
+  tags: string
+  body: string
+  noTags: string
+  noNotes: string
+}
+
+interface OnDeleteNote {
+  title: string
+  cancel: string
+  ok: string
+  text: string
+}
+
+interface OnDeleteTag {
+  title: string
+  cancel: string
+  ok: string
+  text: string
 }
 
 interface Main {
+  notes: string
+  newNote: string
   title: string
   tags: string
   body: string
   save: string
+  edit: string
+  editTags: string
   cancel: string
+  delete: string
+  success: string
 }
 
 export const langs: Langs = {
   en: {
     main: {
+      notes: "Notes",
+      newNote: "New Note",
       title: "Title",
       tags: "Tags",
-      body: "Body",
+      body: "Text",
       save: "Save",
+      edit: "Edit",
+      editTags: "Edit Tags",
       cancel: "Cancel",
+      delete: "Delete",
+      success: "Success!",
+    },
+    error: {
+      title: "Please enter a title between 3 and 70 symbols.",
+      tags: "Please enter between 1 and 6 tags.",
+      body: "Please enter a body text between 5 and 5000 symbols.",
+      noTags: "You have no saved tags. Please make at least one note to change that.",
+      noNotes: "You have no saved Notes. Please make at least one note to change that.",
+    },
+    onDeleteNote: {
+      title: "Delete",
+      cancel: "Cancel",
+      ok: "OK",
+      text: "Do you want to delete this note?",
+    },
+    onDeleteTag: {
+      title: "Delete",
+      cancel: "Cancel",
+      ok: "OK",
+      text: "Do you want to delete this tag?",
     },
   },
 
   bg: {
     main: {
+      notes: "Записки",
+      newNote: "Нова записка",
       title: "Заглавие",
-      tags: "Категории",
+      tags: "Групи",
       body: "Текст",
       save: "Запази",
-      cancel: "Cancel",
+      edit: "Промени",
+      editTags: "Групи",
+      cancel: "Назад",
+      delete: "Изтрий",
+      success: "Успешно!",
+    },
+    error: {
+      title: "Моля въведете заглавие с дължина между 3 и 70 символа.",
+      tags: "Моля въведете от 1 до 6 групи.",
+      body: "Моля въведете текст с дължина между 5 и 5000 символа.",
+      noTags: "Нямате запазени групи. Направете поне една записка за да промените това.",
+      noNotes: "Нямате запазени записки. Направете поне една записка за да промените това.",
+    },
+    onDeleteNote: {
+      title: "Изтриване",
+      cancel: "Не",
+      ok: "Да",
+      text: "Желаете ли да изтриете тази записка?",
+    },
+    onDeleteTag: {
+      title: "Изтриване",
+      cancel: "Не",
+      ok: "Да",
+      text: "Желаете ли да изтриете тази категория?",
     },
   },
 }
