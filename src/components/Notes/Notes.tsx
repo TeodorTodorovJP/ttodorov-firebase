860df61924d87bac71d88c854ecb434105f9a2da
Teodor Todorov <ttodorov.jp@gmail.com>
2023-11-11 06:22:34 +0200

    Refactored Notes
    Not it acts as a main wrapper
    
    The main role is to give a general layout for the Notes app

--- src/components/Notes/Notes.tsx
+++ src/components/Notes/Notes.tsx
@@ -1,95 +1,146 @@
 import { Langs, langs } from "./notesTexts"
 import { useAppDispatch, useAppSelector } from "../../app/hooks"
-import { selectImageBlobUrl, selectUserPreferences, Image } from "../Auth/userSlice"
+import { selectImageBlobUrl, selectUserPreferences, Image, selectUserData } from "../Auth/userSlice"
 import useError from "../CustomHooks/useError"
-import { useEffect, useMemo, useState } from "react"
-import { getBlobUrl, getDateDataInUTC } from "../../app/utils"
-import { Box, Button, Link, Typography, useMediaQuery } from "@mui/material"
+import { useEffect, useMemo, useState, useRef } from "react"
+import {
+  Autocomplete,
+  Box,
+  Button,
+  CircularProgress,
+  InputLabel,
+  Link,
+  OutlinedInput,
+  Stack,
+  TextField,
+  Typography,
+  useMediaQuery,
+} from "@mui/material"
 
 import { Outlet, useNavigate } from "react-router-dom"
 import { Preview } from "@mui/icons-material"
+import { useGetNotesQuery, useGetTagsQuery } from "./notesApi"
+import { NoteData, selectNotes, selectTags, Tag } from "./notesSlice"
+import NotesList from "./NotesList"
 
 type AboutButtons = "aboutMe" | "skills" | "experience"
-
-export type Note = {
-  id: string
-} & NoteData
-
-export type RawNote = {
-  id: string
-} & RawNoteData
-
-export type RawNoteData = {
-  title: string
-  markdown: string
-  tagIds: string[]
-}
-
-export type NoteData = {
-  title: string
-  markdown: string
-  tags: Tag[]
-}
-
-export type Tag = {
-  id: string
-  label: string
-}
 
 export const Notes = () => {
   const { lang: currentLang } = useAppSelector(selectUserPreferences)
   const dispatch = useAppDispatch()
   const images = useAppSelector(selectImageBlobUrl)
+  const currentUser = useAppSelector(selectUserData)
+  const notes = useAppSelector(selectNotes)
+  const tags = useAppSelector(selectTags)
+
+  // Use the default user id of guestuser@abvg.bg
+  useGetTagsQuery(currentUser.id)
 
   const [imageData, setImageData] = useState<Image | null>(null)
   const [openSnack, setOpenSnack] = useState<string | null>(null)
   const [aboutButton, setAboutButton] = useState<AboutButtons>("aboutMe")
+  const [tempTags, setTempTags] = useState<Tag[]>([])
+  const [title, setTitle] = useState<string>("")
 
-  const [notes, setNotes] = useState<RawNote[]>([])
-  const [tags, setTags] = useState<Tag[]>([])
+  /** Get messages for the current room from firebase */
 
-  const notesWithTags = useMemo(() => {
-    return notes.map((note) => {
-      return { ...note, tags: tags.filter((tag) => note.tagIds.includes(tag.id)) }
-    })
-  }, [notes, tags])
-
-  const onCreateNote = ({ tags, ...data }: NoteData) => {
-    const { formattedDate: timestamp } = getDateDataInUTC()
-
-    setNotes((prevNotes) => {
-      return [...prevNotes, { ...data, id: timestamp, tagIds: tags.map((tag) => tag.id) }]
-    })
-  }
-
-  const { main } = langs[currentLang as keyof Langs]
+  const { main, error } = langs[currentLang as keyof Langs]
 
   // @ts-ignore
   const isSmallScreen = useMediaQuery((theme) => theme.breakpoints.down("md"))
 
+  const onTagsChange = (event: React.SyntheticEvent<Element, Event>, values: Tag[]) => {
+    setTempTags(values)
+  }
+
+  const getLabels = (option: string | Tag) => {
+    if (typeof option === "string") {
+      return option
+    } else if (!!option && !!option.title) {
+      return option.title
+    } else {
+      return ""
+    }
+  }
+
+  const filteredNotes = useMemo(() => {
+    return notes.filter((note) => {
+      return (
+        (title === "" || note.title.toLocaleLowerCase().includes(title?.toLocaleLowerCase())) &&
+        (tempTags.length === 0 || tempTags.every((tag) => note.tags.some((noteTag) => noteTag.title === tag.title)))
+      )
+    })
+  }, [title, tempTags, notes])
+
   /** Access Router */
   const navigate = useNavigate()
+
+  useGetNotesQuery(currentUser.id)
+
+  if (!notes) {
+    return (
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    )
+  }
+
+  if (!tags) {
+    return (
+      <Box sx={{ display: "flex" }}>
+        <CircularProgress />
+      </Box>
+    )
+  }
 
   return (
     <Box
       sx={{
         display: "flex",
         flexDirection: "column",
-        position: "fixed",
+        position: "relative",
         // The general sizing of the notes page
         maxWidth: "100vw",
-        maxHeight: "100vh",
         minWidth: "100vw",
-        minHeight: { xs: "90vh", md: "80vh" },
-        top: { xs: "8vh", md: "initial" },
+
+        // toolbar (navigation) maxHeight: { xs: "64px", md: "110px" }
+        top: "64px",
+        maxHeight: "calc(100vh - 64px)",
+        minHeight: "calc(100vh - 64px)",
+        //minHeight: { xs: "90vh", md: "80vh" },
+        //top: { xs: "8vh", md: "initial" },
       }}
     >
-      <Typography>Hello</Typography>
-      <Button onClick={() => navigate("/notes/new")}> New Note </Button>
-
       <Outlet />
     </Box>
   )
 }
 
 export default Notes
+
+
+
+
+
+
+/*
+
+npm install --save-dev @babel/plugin-proposal-private-property-in-object
+
+
+One of your dependencies, babel-preset-react-app, is importing the
+"@babel/plugin-proposal-private-property-in-object" package without        
+declaring it in its dependencies. This is currently working because        
+"@babel/plugin-proposal-private-property-in-object" is already in your     
+node_modules folder for unrelated reasons, but it may break at any time.   
+
+babel-preset-react-app is part of the create-react-app project, which      
+is not maintianed anymore. It is thus unlikely that this bug will
+ever be fixed. Add "@babel/plugin-proposal-private-property-in-object" to  
+your devDependencies to work around this error. This will make this message
+go away.
+
+
+
+
+*/
