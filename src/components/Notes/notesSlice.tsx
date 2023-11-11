ed3def4bf5355d75793ca0c90af4ca93141e8e38
Teodor Todorov <ttodorov.jp@gmail.com>
2023-11-11 06:32:17 +0200

    Notes Slice
    Created CRUD actions for the tags and notes

--- src/components/Notes/notesSlice.tsx
+++ src/components/Notes/notesSlice.tsx
@@ -1,12 +1,13 @@
 import { createAsyncThunk, createSlice, PayloadAction, ThunkAction } from "@reduxjs/toolkit"
 import { RootState, AppThunk, AppDispatch } from "../../app/store"
-import { getDateDataInUTC } from "../../app/utils"
 import { Langs } from "./notesTexts"
+import { v4 as uuid } from "uuid"
 
 /** Set's the default language of the app. */
 export const defaultLang: keyof Langs = "en"
 
 export type NoteData = {
+  userId: string
   id: string
   title: string
   markdown: string
@@ -15,16 +16,20 @@ export type NoteData = {
 
 export type Tag = {
   id: string
-  label: string
+  title: string
 }
 
 export interface NotesState {
   notes: NoteData[]
+  tags: Tag[]
 }
+
+const initialTagValue = { id: uuid(), title: "New Tag" }
 
 const getMainInitialState = () => {
   const newState: NotesState = {
     notes: [],
+    tags: [],
   }
   return newState
 }
@@ -36,13 +41,46 @@ export const notesSlice = createSlice({
   initialState,
   // The `reducers` field lets us define reducers and generate associated actions
   reducers: {
-    /** Opens a menu, passed to the item param. */
-    addNote: (state, action: PayloadAction<NoteData>) => {
-      const { formattedDate: timestamp } = getDateDataInUTC()
-      action.payload.id = timestamp
+    addTags: (state, action: PayloadAction<Tag[]>) => {
+      //state.notes = [ ...state.notes,  action.payload.noteData]
+      state.tags = [...state.tags, ...action.payload]
+    },
 
+    setTags: (state, action: PayloadAction<Tag[]>) => {
+      //state.notes = [ ...state.notes,  action.payload.noteData]
+      // Provide an initial tag for each user.
+      // if (action.payload.length < 1) {
+      //   state.tags = [{ id: "initial", title: "New" }]
+      // } else {
+      state.tags = action.payload
+      //}
+    },
+    addNote: (state, action: PayloadAction<NoteData>) => {
       //state.notes = [ ...state.notes,  action.payload.noteData]
       state.notes.push(action.payload)
+    },
+
+    deleteNote: (state, action: PayloadAction<{ id: string }>) => {
+      //state.notes = [ ...state.notes,  action.payload.noteData]
+      state.notes.filter((note) => note.id === action.payload.id)
+    },
+
+    setNotes: (state, action: PayloadAction<NoteData[]>) => {
+      //state.notes = [ ...state.notes,  action.payload.noteData]
+      // Provide an initial tag for each user.
+      // if (action.payload.length < 1) {
+      //   state.notes = [
+      //     {
+      //       id: "initial",
+      //       title: "New Note",
+      //       userId: "initial",
+      //       markdown: "Text",
+      //       tags: [initialTagValue],
+      //     },
+      //   ]
+      // } else {
+      state.notes = action.payload
+      //}
     },
 
     /** Reset's the navigation state to the initial values. */
@@ -50,12 +88,13 @@ export const notesSlice = createSlice({
   },
 })
 
-export const { addNote, clearNotesData } = notesSlice.actions
+export const { addNote, setNotes, addTags, setTags, clearNotesData, deleteNote } = notesSlice.actions
 
 // The function below is called a selector and allows us to select a value from
 // the state. Selectors can also be defined inline where they're used instead of
 // in the slice file. For example: `useSelector((state: RootState) => state.counter.value)`
 export const selectNote = (state: RootState, id: string) => state.notes.notes.filter((note) => note.id === id)
 export const selectNotes = (state: RootState) => state.notes.notes
+export const selectTags = (state: RootState) => (state.notes.tags.length === 0 ? [initialTagValue] : state.notes.tags)
 
 export default notesSlice.reducer
