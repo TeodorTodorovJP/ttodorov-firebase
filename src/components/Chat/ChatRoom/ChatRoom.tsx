b8f9984e6e0a7de1c5301b97fb358def637598cb
Teodor Todorov <ttodorov.jp@gmail.com>
2023-11-11 05:33:37 +0200

    Css Fix
    MUI compiler does not like css selectors with dashes
    
    web-kit-box-shadow converted to webKitBoxShadow

--- src/components/Chat/ChatRoom/ChatRoom.tsx
+++ src/components/Chat/ChatRoom/ChatRoom.tsx
@@ -504,7 +504,7 @@ export const ChatRoom = (props: { room: ChatRoomsContent; notifyForMessages: Fun
             width: "0.4em",
           },
           "&::-webkit-scrollbar-track": {
-            "-webkit-box-shadow": "inset 0 0 6px rgba(0,0,0,0.00)",
+            webkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
           },
           "&::-webkit-scrollbar-thumb": {
             backgroundColor: "primary",
