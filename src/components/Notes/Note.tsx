f74f8faf70ead3a38b54e6d62db259b75ee01101
Teodor Todorov <ttodorov.jp@gmail.com>
2023-11-12 18:55:44 +0200

    Documentation

--- src/components/Notes/Note.tsx
+++ src/components/Notes/Note.tsx
@@ -1,12 +1,15 @@
-import { Box, Card, CardActionArea, CardContent, CardMedia, Chip, Paper, Typography } from "@mui/material"
+import { Box, Card, Chip, Typography } from "@mui/material"
 import { memo } from "react"
 import { NoteData } from "./notesSlice"
-import { Outlet, useNavigate, Link as RouterLink } from "react-router-dom"
+import { Link as RouterLink } from "react-router-dom"
 
-export const Note = memo(({ userId, id, title, markdown, tags }: NoteData) => {
-  /** Access Router */
-  const navigate = useNavigate()
-
+/**
+ * Note Component
+ *
+ * Main goal is to display each note on the 'NotesBrowse' screen.
+ *
+ */
+export const Note = memo(({ id, title, tags }: NoteData) => {
   return (
     <Card
       key={id}
@@ -18,8 +21,8 @@ export const Note = memo(({ userId, id, title, markdown, tags }: NoteData) => {
         maxWidth: { xs: "93vw", sm: "40vw", md: "25vw", lg: "15vw" },
         minWidth: { xs: "93vw", sm: "40vw", md: "25vw", lg: "15vw" },
 
-        maxHeight: { xs: "93vw", sm: "40vw", md: "25vw", lg: "15vw" },
-        minHeight: { xs: "93vw", sm: "40vw", md: "25vw", lg: "15vw" },
+        maxHeight: { xs: "30vw", sm: "40vw", md: "25vw", lg: "15vw" },
+        minHeight: { xs: "30vw", sm: "40vw", md: "25vw", lg: "15vw" },
 
         display: "flex",
         flexDirection: "column",
@@ -27,6 +30,7 @@ export const Note = memo(({ userId, id, title, markdown, tags }: NoteData) => {
         justifyContent: "center",
         transition: "ease 0.5s",
         textDecoration: "none",
+        marginBottom: "5vh",
 
         "&:hover": {
           boxShadow: 20,
@@ -41,7 +45,7 @@ export const Note = memo(({ userId, id, title, markdown, tags }: NoteData) => {
       </Typography>
       <Box sx={{ display: "flex", flexDirection: "row", flexWrap: "wrap", gap: "10px" }}>
         {tags.map((tag) => {
-          return <Chip key={tag.id} label={tag.title} color="primary" />
+          return <Chip key={tag} label={tag} color="primary" />
         })}
       </Box>
     </Card>
