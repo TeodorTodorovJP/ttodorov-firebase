51a412a95adb23f3840b2b45bfdf57ddd7e87644
Teodor Todorov <ttodorov.jp@gmail.com>
2023-11-12 18:53:16 +0200

    Documentation

--- src/components/Notes/EditNote.tsx
+++ src/components/Notes/EditNote.tsx
@@ -3,6 +3,13 @@ import { useAppSelector } from "../../app/hooks"
 import NoteForm from "./NoteForm"
 import { selectNote } from "./notesSlice"
 
+/**
+ * EditNote Component
+ *
+ * Main goal is to wrap the 'NoteForm' Component with data. 
+ * Get's the data for a note, based on the id passed to the route params.
+ *
+ */
 export const EditNote = () => {
   const params = useParams<"id">()
   const noteData = useAppSelector((state) => selectNote(state, params.id!)[0])
